1604c61eb10b678cebb2f137b11fa70a2c9d6c69
Vishva265 <valandvishva914@gmail.com>
2025-07-04 09:53:27 +0530

    Practical3

---
+++ welcome/src/App.js
@@ -0,0 +1,26 @@
+
+import React, { useState, useEffect } from 'react';
+import './App.css';
+
+function App() {
+  const [currentTime, setCurrentTime] = useState(new Date());
+
+  useEffect(() => {
+    const timer = setInterval(() => {
+      setCurrentTime(new Date());
+    }, 1000);
+
+    // Cleanup on component unmount
+    return () => clearInterval(timer);
+  }, []);
+
+  return (
+    <div className="container">
+      <h1 className="title">Welcome to CHARUSAT!!!!</h1>
+      <h2 className="date">It is {currentTime.toLocaleDateString()}</h2>
+      <h2 className="time">It is {currentTime.toLocaleTimeString()}</h2>
+    </div>
+  );
+}
+
+export default App;
